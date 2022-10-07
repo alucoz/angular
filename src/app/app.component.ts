586dea78a18296648aae985a83f28fe5bdacd10a
Wikor <saber.kh730@gmail.com>
2022-10-07 14:20:55 +0200

    dodawanie nowych zadan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,12 +22,18 @@ get username(): string{
 };
 
 get itemCount(): number{
-   return this.list.items.filter(item => !item.complete).length;
+   return this.list.items.length;
 
 
 }
 
 get items(): readonly TodoItem[]{
-  return this.list.items;
+  return this.list.items.filter(item => !item.complete);
 }
+
+addItem(newItem: string){
+  if (newItem != ""){
+    this.list.addItem(newItem);
+    }
+  }
 }
